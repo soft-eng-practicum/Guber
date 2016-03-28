51ebb59ff06ad83c777e7d1c31a47e74a65fd84f
mstackpo <mstackpo@ggc.edu>
2016-03-27 20:01:39 -0400

    Karma tests working, added google-maps-mock.js to mock google service.

--- GuberApp/tests/angularApp-spec.js
+++ GuberApp/tests/angularApp-spec.js
@@ -3,17 +3,17 @@ describe('Distance Controller', function() {
   var DistCtrl = null;
   var scope = null;
 
-  beforeEach(module('guber', ['ui.router']));
+  beforeEach(module('guber'));
 
-  // beforeEach(inject(function ($rootScope, $controller) {
-  //   scope = $rootScope.$new();
-  //   DistCtrl = $controller('DistCtrl', {
-  //     $scope: scope
-  //   });
-  // }));
+  beforeEach(inject(function ($rootScope, $controller) {
+    scope = $rootScope.$new();
+    DistCtrl = $controller('DistCtrl', {
+      $scope: scope
+    });
+  }));
 
   it('Say GGC', function () {
-    expect($scope.ggc).toEqual("Georgia Gwinnett College");
+    expect(scope.ggc).toEqual("Georgia Gwinnett College");
   });
 
 });
